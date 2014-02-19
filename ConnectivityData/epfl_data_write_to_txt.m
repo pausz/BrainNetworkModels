e6d2fbbe7a1394f2ee3fb54672b8f3f2870404b8
Paula Sanz Leon <pau.sleon@gmail.com>
2014-02-19 10:53:51 +0100

    Add code cells for each possible case that we can obtain from the epfl dataset, it's better than changing parameters

--- ConnectivityData/epfl_data_write_to_txt.m
+++ ConnectivityData/epfl_data_write_to_txt.m
@@ -5,17 +5,153 @@
 % # average connectivities '4 '
 % # 164 new connectivities.
 
-for s=1:40,
+
+%%
+% parcellation : low
+% subject      : average 
+% subcortical  : true
+% brainstem    : false
+% yields       : 82ROIs matrix
     
     options.Connectivity.WhichMatrix = 'EPFL';
     options.Connectivity.Parcellation = 'low';
-    options.Connectivity.WhichSubject = 'individual';
+    options.Connectivity.WhichSubject = 'average';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+
+%%
+% parcellation : low
+% subject      : average 
+% subcortical  : false
+% brainstem    : false
+% yields       : 68ROIs matrix
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'low';
+    options.Connectivity.WhichSubject = 'average';
     options.Connectivity.RemoveThalamus = true;
     options.Connectivity.invel = 1;
     options.Connectivity.subject = s;
     options.Connectivity = GetConnectivity(options.Connectivity);
     
     write_connectivity_to_txt(options.Connectivity)
+        
+%%
+% parcellation : low
+% subject      : individual 
+% subcortical  : true
+% brainstem    : false
+% yields       : 40 x 68ROIs matrices
+
+for s=1:40,
     
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'low';
+    options.Connectivity.WhichSubject = 'average';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
     
+    write_connectivity_to_txt(options.Connectivity)
+        
+end
+
+%%
+% parcellation : low
+% subject      : individual 
+% subcortical  : true
+% brainstem    : false
+% yields       : 40 x 828ROIs matrices
+for s=1:40,
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'low';
+    options.Connectivity.WhichSubject = 'individual';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+        
+end
+
+%%
+% parcellation : high
+% subject      : average 
+% subcortical  : true
+% brainstem    : false
+% yields       : 82ROIs matrix
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'high';
+    options.Connectivity.WhichSubject = 'average';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+
+%%
+% parcellation : high
+% subject      : average 
+% subcortical  : false
+% brainstem    : false
+% yields       : 68ROIs matrix
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'high';
+    options.Connectivity.WhichSubject = 'average';
+    options.Connectivity.RemoveThalamus = true;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+        
+%%
+% parcellation : high
+% subject      : individual 
+% subcortical  : true
+% brainstem    : false
+% yields       : 40 x 68ROIs matrices
+
+for s=1:40,
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'high';
+    options.Connectivity.WhichSubject = 'average';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+        
+end
+
+%%
+% parcellation : high
+% subject      : individual 
+% subcortical  : true
+% brainstem    : false
+% yields       : 40 x 828ROIs matrices
+for s=1:40,
+    
+    options.Connectivity.WhichMatrix = 'EPFL';
+    options.Connectivity.Parcellation = 'high';
+    options.Connectivity.WhichSubject = 'individual';
+    options.Connectivity.RemoveThalamus = false;
+    options.Connectivity.invel = 1;
+    options.Connectivity.subject = s;
+    options.Connectivity = GetConnectivity(options.Connectivity);
+    
+    write_connectivity_to_txt(options.Connectivity)
+        
 end
