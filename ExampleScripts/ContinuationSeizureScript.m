1570f45c4fdfa6be6b3c6e0ba8f9c6759069fc43
Paula Sanz-Leon <p.sanz-leon@physics.usyd.edu.au>
2015-07-08 13:37:39 +1000

    Examples: reproduce seizure onset and offset for the spatially uniform case of BRRW

--- ExampleScripts/ContinuationSeizureScript.m
+++ ExampleScripts/ContinuationSeizureScript.m
@@ -1,11 +1,23 @@
-%% Quick and dirty version, without smooth transition in and out of
-%% seizure for the global case
-%
-% Approximate runtime: XX workstation circa 2012
-% Approximate memory: XX
+%% Transition in and out of an absence seizure for the spatially uniform case as presented in 
+% [1] Breakspear et al., (2005 
+% A Unifying Explanation of Primary Generalized Seizures Through Nonlinear Brain Modelling and Bifurcation Analysis.
+% Crebral Cortex
+
+% Approximate runtime: 80 [s] workstation March 2015
+% Approximate memory:  500 MB
 % Approximate storage: XX
 
+%% Description of the simulation
+% This simulation intends to reproduce key results shown in Figure 3 of
+% [1].
+% 
+% a) There are two uncoupled nodes, so in principle, their dynamics should be identical.
+% b) The model is initialized with the parameters for an absence seizure as
+%    described in Table 1 in [1].
+% c) The connectivity parameter 'nu_se' is increased from a base value to an 
+%    end value in 3 steps.   
 
+tic;
 %% Connectivity
  options.Connectivity.WhichMatrix = 'Random';
  options.Connectivity.NumberOfNodes = 2;
@@ -30,13 +42,11 @@ options.Integration.iters = 2^14;
 options = SetDerivedParameters(options);
 options = SetInitialConditions(options); 
 
-%% Check phi_n
-plot(options.Dynamics.phi_n)
-
-%%Seizure values
-Base_nu_se = 1000;
-Middle_nu_se = 2000;
-Seizure_nu_se = 4400;
+%% Nu_se values
+LinearlyStable_nu_se = 1000;
+Supercritical_nu_se  = 1800;
+FirstPeriodDoubling_nu_se = 3400;
+SecondPeriodDoubling_nu_se = 4200;
 SeizureRegion = '1';
 SeizureRegionIndex = find(strcmpi(options.Connectivity.NodeStr, SeizureRegion));
  
@@ -44,24 +54,21 @@ SeizureRegionIndex = find(strcmpi(options.Connectivity.NodeStr, SeizureRegion));
  % Increased verbosity, here it enables the integration step counter.
  % options.Other.verbosity = 1;
  
- 
-options.Dynamics.nu_se = Base_nu_se;
+options.Dynamics.nu_se = LinearlyStable_nu_se;
 options = SetInitialConditions(options);
 
 
 %% Output
-DSF_t = 1;  %with dt = 2^-3 and iters 2^14 each continuation has a duration of 2048ms 
-DSF_x = 1;
+DSF_t = 1;  %with dt = 2^-3 and iters 2^14 each continuation has a durationof 2048ms 
+cont = 16;  % total number of continuations to store 
+tpts = (options.Integration.iters / DSF_t) * cont;
 Store_phi_e = [];
-Store_phi_e = [];
- 
  
 %% Initial integration, to clear transient
  Continuations = 8;
  for k = 1:Continuations,
    disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for initial integration...'])
-   %options.Dynamics.phi_n = 1e-3.*ones(options.Integration.iters+options.Integration.maxdelayiters, options.Connectivity.NumberOfNodes);
-   [phi_e dphi_e V_e dV_e V_s dV_s V_r dV_r t options] = BRRW_heun(options);
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
    options = UpdateInitialConditions(options);
    
  end
@@ -71,48 +78,56 @@ Store_phi_e = [];
  Continuations = 2;
  for k = 1:Continuations,
    disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for pre seizure...'])
-   %options.Dynamics.phi_n = 1e-3.*ones(options.Integration.iters+options.Integration.maxdelayiters, options.Connectivity.NumberOfNodes);
-   [phi_e dphi_e V_e dV_e V_s dV_s V_r dV_r t options] = BRRW_heun(options);
-   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end,1:DSF_x:end)];
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
+   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end,:)];
    options = UpdateInitialConditions(options);
  end
 
 
-%% Seizure Middle
- options.Dynamics.nu_se(:) =  Middle_nu_se;
- Continuations = 8;
+%% Supercritical instability
+ options.Dynamics.nu_se(:) =  Supercritical_nu_se;
+ Continuations = 4;
  for k = 1:Continuations,
    disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for middle seizure...'])
-   %options.Dynamics.phi_n = 1e-3.*ones(options.Integration.iters+options.Integration.maxdelayiters, options.Connectivity.NumberOfNodes);
-   [phi_e dphi_e V_e dV_e V_s dV_s V_r dV_r t options] = BRRW_heun(options);
-   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, 1:DSF_x:end)];
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
+   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, :)];
    options = UpdateInitialConditions(options);
  end
 
 
-%% Seizure Full
- options.Dynamics.nu_se(:) =  Seizure_nu_se; 
- Continuations = 8;
+%% First Period Doubling
+ options.Dynamics.nu_se(:) =  FirstPeriodDoubling_nu_se; 
+ Continuations = 4;
  for k = 1:Continuations,
    disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for full seizure...'])
-   %options.Dynamics.phi_n = 1e-3.*ones(options.Integration.iters+options.Integration.maxdelayiters, options.Connectivity.NumberOfNodes);
-   [phi_e dphi_e V_e dV_e V_s dV_s V_r dV_r t options] = BRRW_heun(options);
-   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, 1:DSF_x:end)];
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
+   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, :)];
+   options = UpdateInitialConditions(options);
+ end
+ 
+ 
+%% Second Period Doubling
+ options.Dynamics.nu_se(:) =  SecondPeriodDoubling_nu_se; 
+ Continuations = 4;
+ for k = 1:Continuations,
+   disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for full seizure...'])
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
+   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, :)];
    options = UpdateInitialConditions(options);
  end
 
  
 %% Post seizure
- options.Dynamics.nu_se = Base_nu_se;
+ options.Dynamics.nu_se =LinearlyStable_nu_se;
  Continuations = 2;
  for k = 1:Continuations,
    disp(['Continuation ' num2str(k) ' of ' num2str(Continuations) ' for post seizure...'])
-   %options.Dynamics.phi_n = 1e-3.*ones(options.Integration.iters+options.Integration.maxdelayiters, options.Connectivity.NumberOfNodes);
-   [phi_e dphi_e V_e dV_e V_s dV_s V_r dV_r t options] = BRRW_heun(options);
-   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end,1:DSF_x:end)];
+   [phi_e, dphi_e, V_e, dV_e, V_s, dV_s, V_r, dV_r, t, options] = BRRW_heun(options);
+   Store_phi_e = [Store_phi_e ; phi_e(1:DSF_t:end, :)];
    options = UpdateInitialConditions(options);
  end
 
+toc;
 
 %% Plot
 
