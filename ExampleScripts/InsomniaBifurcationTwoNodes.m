1b37c25750434c10427819cfc74d71a50e546b9c
Paula Sanz-Leon <p.sanz-leon@physics.usyd.edu.au>
2017-07-03 13:58:01 +1000

    Clean up repo

---
+++ ExampleScripts/InsomniaBifurcationTwoNodes.m
@@ -0,0 +1,84 @@
+%% Calculate bifurcation as a function of nu_se for BRRW-absence.
+%
+% Calculation is done using NodeBifurcation() and subsequent
+% visualisation uses PlotNodeBifurcation().
+%
+% Approximate runtime: 2h15m , Octave, Workstation circa 2012
+% Approximate memory:  < 500MB
+% Approximate storage: <?>MB 
+% From command line:
+% matlab -nodesktop -nosplash -r AbsenceBifurcationScriptTwoNodes
+
+%% Some details of our environment...
+  %Where is the code
+  CodeDir = '..';        %can be full or relative directory path
+  ScriptDir = pwd;       %get full path to this script
+  cd(CodeDir)            %Change to code directory
+  FullPathCodeDir = pwd; %get full path of CodeDir
+  ThisScript = mfilename; %which script is being run
+  
+  %When and Where did we start:
+  disp(['Script started: ' when()]) 
+  if strcmp(filesep,'/'), %on a *nix machine, write machine details log...
+    system('uname -a') 
+  end 
+  disp(['Running: ' ThisScript])
+  disp(['Script directory: ' ScriptDir])
+  disp(['Code directory: ' FullPathCodeDir])
+ 
+%% Do the stuff...
+  
+  %Specify Connectivity to use
+  options.Connectivity.WhichMatrix = 'Random';
+  options.Connectivity.NumberOfNodes = 2;
+  %options.Connectivity.RemoveThalamus = true;
+  options.Connectivity.invel = 1.0/7.0;
+  
+  %Specify Dynamics to use
+  options.Dynamics.WhichModel = 'BRRW';
+  options.Dynamics.BrainState = 'ha';
+  %%
+  %Load default parameters for specified connectivity and dynamics
+  options.Connectivity = GetConnectivity(options.Connectivity);
+  options.Dynamics = SetDynamicParameters(options.Dynamics);
+  options = SetIntegrationParameters(options);
+  
+  options.Integration.dt = 2^-14;
+  options.Integration.iters = 2^12;
+  
+  options = SetDerivedParameters(options);
+  options = SetInitialConditions(options);
+  
+  addpath(genpath('./Bifurcations'))
+  options.Bifurcation.BifurcationParameter='nu_sn';
+  options = SetBifurcationOptions(options);
+  options.Bifurcation.BifurcationParameterIncrement =  0.005e2;
+  options.Bifurcation.ErrorTolerance = 1.0e-6; 
+  options.Bifurcation.MaxContinuations = 3;
+  options.Bifurcation.AttemptForceFixedPoint = false;
+  
+  addpath(genpath('./PlottingTools')) %Need this if using interactive mode
+  options.Other.verbosity = 4; %42;
+  
+  %Calcualte the bifurcation
+  [ForwardFxdPts, BackwardFxdPts, options] = NodeBifurcation(options);
+
+
+%% When did we finish:
+  disp(['Script ended: ' when()])
+%% Save the results
+  save(ThisScript, 'ForwardFxdPts', 'BackwardFxdPts', 'options')
+%% 
+exit
+%% Plotting
+  %% Select a few nodes
+  options.Plotting.OnlyNodes = {'1'};
+  
+  % plot them
+  FigureHandles = PlotNodeBifurcation(ForwardFxdPts, options);
+  %%
+  %Optionally over plot the Extrema found by back tracking
+  options.Plotting.FigureHandles = FigureHandles;
+  FigureHandles = PlotNodeBifurcation(BackwardFxdPts, options);
+
+%%%EoF%%%
